bf33476778aafa6846646791aeaeefbbba3c6d57
Steve Sloka <steve@pulumi.com>
2022-04-22 13:40:01 -0400

    remove fargate example
    Signed-off-by: Steve Sloka <steve@pulumi.com>

--- index.ts
+++ index.ts
@@ -8,13 +8,3 @@ const repository = new awsx.ecr.Repository("sample");
 // this program. Once built, push that image up to our personal ECR repo.
 const image = repository.buildAndPushImage("./app")
 
-const service = new awsx.ecs.FargateService("service", {
-    // ... common code from before trimmed out
-    taskDefinitionArgs: {
-        containers: {
-            service: {
-                image: image,
-            },
-        },
-    },
-});
